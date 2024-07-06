4c91e511105d46aa70d1a0c3cfb62d235dc88de7
Ronil Patil <98299193+Ronilpatil22@users.noreply.github.com>
2024-07-06 18:27:12 +0530

    select and play song in player

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import Sidebar from './components/Sidebar'
 import Player from './components/Player'
 import Display from './components/Display'
+import { useContext, useRef } from 'react'
+import { PlayerContext } from './context/PlayerContext'
 const App = () => {
+
+  const {audioRef,track} = useContext(PlayerContext)
+
   return (
     <div className='h-screen bg-black'>
       <div className='h-[90%] flex'>
@@ -9,6 +14,7 @@ const App = () => {
         <Display />
       </div>
       <Player></Player>
+      <audio ref={audioRef} src={track.file} preload='auto'></audio>
     </div>
   )
 }
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,27 +1,33 @@
-import { assets, songsData } from '../assets/assets'
-
+import { assets } from '../assets/assets'
+import { PlayerContext } from '../context/PlayerContext'
+import { useContext } from 'react'
 const Player = () => {
+
+    const {seekBar,track,seekBg,playStatus,play,pause} = useContext(PlayerContext)
+     
+
   return (
     <div className='h-[10%] bg-black flex justify-between items-center text-white px-4'>
         <div className='hidden lg:flex items-center gap-4'>
-            <img className='w-12' src={songsData[0].image}></img>
+            <img className='w-12' src={track.image}></img>
             <div>
-                <p>{songsData[0].name}</p>
-                <p>{songsData[0].desc.slice(0,12)}</p>
+                <p>{track.name}</p>
+                <p>{track.desc.slice(0,12)}</p>
             </div>
         </div>
         <div className='flex flex-col items-center gap-1 m-auto'>
             <div className='flex gap-4'>
                 <img className='w-4 cursor-pointer' src={assets.shuffle_icon}></img>
                 <img className='w-4 cursor-pointer' src={assets.prev_icon}></img>
-                <img className='w-4 cursor-pointer' src={assets.play_icon}></img>
+                {!playStatus && <img onClick={play} className='w-4 cursor-pointer' src={assets.play_icon}></img>}
+                {playStatus && <img onClick={pause} className='w-4 cursor-pointer' src={assets.pause_icon}></img>}
                 <img className='w-4 cursor-pointer' src={assets.next_icon}></img>
                 <img className='w-4 cursor-pointer' src={assets.loop_icon}></img>
             </div>
             <div className='flex items-center gap-5'>
                 <p>1:06</p>
-                <div className='w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer'>
-                    <hr className='h-1 border-none w-10 bg-green-800 rounded-full'>
+                <div redf={seekBg} className='w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer'>
+                    <hr ref={seekBar} className='h-1 border-none w-10 bg-green-800 rounded-full'>
                     </hr>
                 </div>
                 <p>3:20</p>
